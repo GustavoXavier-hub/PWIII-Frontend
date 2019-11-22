53e1a213459c2eb5cd2bd29fbf30fc338254e858
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-11-22 20:15:06 -0300

    Configuração do bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+import { NovoPessoaComponent } from './novo-pessoa/novo-pessoa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listagem', component: ListaPessoaComponent},
+  {path: 'novo', component: NovoPessoaComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
