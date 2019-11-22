10c467fae753ad0846a855eab48d42362f57fe9a
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-11-22 19:52:56 -0300

    Criaçao-front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+
+import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPessoaComponent,
+    ListaUsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lista-pessoa/lista-pessoa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-lista-pessoa',
+  templateUrl: './lista-pessoa.component.html',
+  styleUrls: ['./lista-pessoa.component.css']
+})
+export class ListaPessoaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
